521e88353b5586d50a96cb0ea77baec965d2f2a0
Ohad Bruker <ohad.bruker@lemonade.com>
2024-02-25 11:10:39 +0200

    produce probes only for synthetic model

--- produce_probes.sh
+++ produce_probes.sh
@@ -1,15 +1,16 @@
 for X in {0..8}
 do
 
-CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --random
-CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --championship
-CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X
+#CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --random
+#CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --championship
+#CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X
 
-for Y in {2,4,8,16,32,64,128,256,512}
+#for Y in {2,4,8,16,32,64,128,256,512}
+for Y in {64,128}
 do
-CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --twolayer --mid_dim $layer --random
-CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --twolayer --mid_dim $layer --championship
-CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --twolayer --mid_dim $layer
+#CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --twolayer --mid_dim $layer --random
+#CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --twolayer --mid_dim $layer --championship
+CUDA_VISIBLE_DEVICES=0 python train_probe_othello.py --layer $X --twolayer --mid_dim $Y
 done
 
 done
